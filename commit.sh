5fcaa193284242d8c00d998008b9961dd02851b1
cjoseph35 <connorschweitzer@gmail.com>
2020-04-27 21:57:22 -0400

    Regular auto-commit at 21:57:22 on 04/27/2020

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-cd "C:\\Users\\CONNO\\OneDrive\\Documents\\GitHub\\COVID\\COVID.Rproj"
+cd C:\\Users\\CONNO\\OneDrive\\Documents\\GitHub\\COVID\\COVID.Rproj
 git add --all
 timestamp() {
   date +"at %H:%M:%S on %m/%d/%Y"
