a2621b5753bf71f8b6c092f457bea3f65d536634
cjoseph35 <connorschweitzer@gmail.com>
2020-04-28 11:52:51 -0400

    Regular auto-commit at 11:52:51 on 04/28/2020

--- taskschedule.R
+++ taskschedule.R
@@ -1,8 +1,5 @@
 library(rmarkdown)
 library(taskscheduleR)
-
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
-
 setwd("C:\\Users\\CONNO\\OneDrive\\Documents\\GitHub\\COVID")
-
 rmarkdown::render_site(encoding = 'UTF-8')
