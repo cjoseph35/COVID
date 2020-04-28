1956783c4916f84b78eef4f2829fe4733637db40
cjoseph35 <connorschweitzer@gmail.com>
2020-04-28 18:02:42 -0400

    Regular auto-commit

--- commit.bat
+++ commit.bat
@@ -1,7 +1,4 @@
 cd C:\\Users\\CONNO\\OneDrive\\Documents\\GitHub\\COVID\\COVID.Rproj
 git add --all
-timestamp() {
-  date +"at %H:%M:%S on %m/%d/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
+git commit -am "Regular auto-commit"
 git push origin master
